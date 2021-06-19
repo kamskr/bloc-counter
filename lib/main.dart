83cf11c64e9822899f25ab3edee96ccca705cfa1
Kamil <sikorakamil26@gmail.com>
2021-06-19 09:48:31 +0200

    added global provideer

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_bloc_concepts/presentation/router/app_router.dart';
+
+import 'logic/cubit/counter_cubit.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
   final AppRouter _appRouter = AppRouter();
 
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return BlocProvider<CounterCubit>(
+      create: (context) => CounterCubit(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        onGenerateRoute: _appRouter.onGenerateRoute,
       ),
-      onGenerateRoute: _appRouter.onGenerateRoute,
     );
-  }
-
-  @override
-  dispose() {
-    _appRouter.dispose();
-    super.dispose();
   }
 }
--- lib/presentation/router/app_router.dart
+++ lib/presentation/router/app_router.dart
@@ -1,42 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_bloc_concepts/logic/cubit/counter_cubit.dart';
 import 'package:flutter_bloc_concepts/presentation/screens/home_screen.dart';
 import 'package:flutter_bloc_concepts/presentation/screens/second_screen.dart';
 import 'package:flutter_bloc_concepts/presentation/screens/third_screen.dart';
 
 class AppRouter {
-  final CounterCubit _counterCubit = CounterCubit();
-
-  // This is how you provide Blocs for specific routes
   Route onGenerateRoute(RouteSettings routeSettings) {
     switch (routeSettings.name) {
       case "/":
         return MaterialPageRoute(
-            builder: (_) => BlocProvider.value(
-                  value: _counterCubit,
-                  child: HomeScreen(title: "Home", color: Colors.blueAccent),
-                ));
+            builder: (_) =>
+                HomeScreen(title: "Home", color: Colors.blueAccent));
         break;
       case "/":
         return MaterialPageRoute(
-            builder: (_) => BlocProvider.value(
-                  value: _counterCubit,
-                  child: SecondScreen(title: "Second", color: Colors.redAccent),
-                ));
+            builder: (_) =>
+                SecondScreen(title: "Second", color: Colors.redAccent));
         break;
       case "/":
         return MaterialPageRoute(
-            builder: (_) => BlocProvider.value(
-                value: _counterCubit,
-                child: ThirdScreen(title: "Third", color: Colors.greenAccent)));
+            builder: (_) =>
+                ThirdScreen(title: "Third", color: Colors.greenAccent));
         break;
       default:
-        return null;
     }
-  }
-
-  void dispose() {
-    _counterCubit.close();
   }
 }
